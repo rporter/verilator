138249f0ab25c1016bae8abd1dd6105d26c589df
Rich Porter <dung.spreader@gmail.com>
2013-10-08 13:39:52 +0100

    revert finish to stop with -n icarus argument

--- test_regress/t/t_vpi_var.v
+++ test_regress/t/t_vpi_var.v
@@ -113,7 +113,7 @@ module arr;
 
    always @(posedge check) begin
      if (verbose) $display("%m : %x %x", sig, rfr);
-     if (check && sig != rfr) $finish;
+     if (check && sig != rfr) $stop;
      check <= 0;
    end
 
